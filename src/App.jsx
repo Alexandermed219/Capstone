eb16c4f3ab813e141c51c6cdd5ad421a707a169b
Alexander Medina <alexandermed219@gmail.com>
2023-08-26 22:34:18 -0400

    Recreated repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [base, setBase] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(json => setBase(json))
+    .catch(error => console.error('Error fetching data:', error));
+  },[]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="store-front">
+      <h1 id="Heading">Transcend Industries</h1>
+      <h2 id="Heading">Explore Products</h2>
+      <ul className='listing'>
+        {base.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
