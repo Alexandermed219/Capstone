61117f6bcac5fa7394af977c272ac0c2755cdf3b
Alexander Medina <alexandermed219@gmail.com>
2023-08-28 18:52:51 -0400

    Added products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import productpage from './components/Product-page';
 
 function App() {
   const [base, setBase] = useState([]);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
+    fetch('https://fakestoreapi.com/products?sort=asc')
       .then(res => res.json())
       .then(json => setBase(json))
       .catch(error => console.error('Error fetching data:', error));
@@ -19,12 +21,21 @@ function App() {
       </div>
       <div className='store-2'>
         <h2 id="Heading">Explore Products</h2>
+        <Router>
+          <Switch>
+            {/* Define your routes */}
+            <Route path="/components/Product-page">
+              <MyPage />
+            </Route>
+            {/* Add more routes as needed */}
+          </Switch>
+        </Router>
       </div>
-      <ul className='listing'>
+      {/* <ul className='listing'>
         {base.map(post => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   )
 }
---
+++ src/components/Product-page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const productpage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Page!</h1>
+      {/* Add your page content here */}
+    </div>
+  );
+};
+
+export default productpage;
