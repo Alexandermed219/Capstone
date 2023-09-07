4bcacb02ed10d4bdfabfbea2faa98cb83d3a6b8a
Alexander Medina <alexandermed219@gmail.com>
2023-09-07 16:25:27 -0400

    Searchbar functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ import './SearchBar.css';
 export const SearchBar = () => {
     return (
         <div className='input-wrapper'>
-        <FaSearch id='search-icon'/>
+        <FaSearch id='search-icon' size={24}/>
          <input placeholder='Search Here' type="text" />
         </div>
     )
