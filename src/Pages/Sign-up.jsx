bf98ab9d5ce76f4833aa1927540d05b21f65edd8
Alexander Medina <alexandermed219@gmail.com>
2023-09-28 14:53:31 -0400

    Finished

--- src/Pages/Sign-up.jsx
+++ src/Pages/Sign-up.jsx
@@ -13,10 +13,8 @@ const Signupform = () => {
       const response = await userName(username, password);
 
       if (response.ok) {
-        // Registration successful, you can handle it here (e.g., show a success message)
         console.log('User registered successfully');
       } else {
-        // Handle registration error (e.g., show an error message)
         console.error('Registration failed');
       }
     } catch (error) {
@@ -26,17 +24,22 @@ const Signupform = () => {
 
   return (
     <div>
-      <h1 className='log-sign-css'><PersonPlus id='nav-icon'/> Sign Up Here</h1>
+      <h1 className='log-sign-css'><PersonPlus id='nav-icon' /> Sign Up Here</h1>
       <div className='sign-container'>
         <form onSubmit={handleSubmit} className='sign-up-form'>
-          <h2 className='user-pass-form'>Create A Username</h2><input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
-          <h2 className='user-pass-form'>Create A Password</h2><input type="password" placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+          <div className='user-pass-form'>
+            <h2>Enter First Name</h2><input type="text" placeholder='Enter First Name' />
+            <h2>Enter Last Name</h2><input type="text" placeholder='Enter Last Name' />
+            <h2>Create A Username</h2><input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+            <h2>Create A Password</h2><input type="password" placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <h2>Enter Your Address</h2><input type="text" placeholder='Enter Your Address' />
+          </div>
           <button className='sub-btn' type='submit'>Register</button>
         </form>
       </div>
       <footer id="copyright">
-                &copy; 2023 Black Label Industries. All Rights Reserved.
-            </footer>
+        &copy; 2023 Black Label Industries. All Rights Reserved.
+      </footer>
     </div>
   );
 };
